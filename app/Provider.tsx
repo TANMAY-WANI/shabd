855e49f6615699462c404874abbb6c0edd4d991d
TANMAY-WANI <tanmaywani145@gmail.com>
2024-09-03 23:47:47 +0530

    add user action for getting mention suggestions

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -2,15 +2,27 @@
 import React from 'react'
 import {LiveblocksProvider, ClientSideSuspense} from '@liveblocks/react/suspense'
 import Loader from '@/components/Loader'
-import { getClerkUsers } from '@/lib/actions/user.actions'
+import { getClerkUsers, getDocumentUsers } from '@/lib/actions/user.actions'
+import { useUser } from '@clerk/nextjs'
 
 const Provider = ({children}: {children:React.ReactNode}) => {
+  const clerkUser = useUser().user
+
   return (
     <LiveblocksProvider authEndpoint="/api/liveblocks-auth"
 
     resolveUsers={ async ({userIds}) =>{
       const users = await getClerkUsers({userIds})
       return users
+    }}
+
+    resolveMentionSuggestions={async ({text,roomId})=>{
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          user:clerkUser?.emailAddresses[0].emailAddress!,
+          text
+        })
+        return roomUsers;
     }}
 
     >
